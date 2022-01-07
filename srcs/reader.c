8d4f586719a108cb6ac0bb6db9b3ded31977bad8
Gabor Horvath-Ulenius <mobahug@gmail.com>
2022-01-07 09:53:11 +0200

    core functions working perfectly and are ready, TO DO: HAVE TO ADD THE VALID PIECES FUNCTION MISSING FROM VALIDATE FUNCTION

--- srcs/reader.c
+++ srcs/reader.c
@@ -6,7 +6,7 @@
 /*   By: ghorvath <ghorvath@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/31 12:35:00 by ghorvath          #+#    #+#             */
-/*   Updated: 2022/01/06 16:07:34 by ghorvath         ###   ########.fr       */
+/*   Updated: 2022/01/07 09:50:45 by ghorvath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,29 +55,29 @@ int	validate(char *str, ssize_t ret)
 	{
 		if (i % 5 != 4 && (str[i] != '#' && str[i] != '.'))
 		{
-			//printf("\n(%d)first_pass\n", i);
+			printf("\n(%d)first_pass\n", i);
 			return (0);
 		}
 		else if (i % 5 == 4 && str[i] != '\n')
 		{
-			//printf("\n(%d)second_pass\n", i);
+			printf("\n(%d)second_pass\n", i);
 			return (0);
 		}
 		if (str[i] == '#' && ++hash_counter < 5)
 		{
-			//printf("\n(%d)third_pass\n", i);
+			printf("\n(%d)third_pass\n", i);
 			total_connections += check_connection(str, i);
-			//printf("(%d)\n", i);
+			printf("(%d)\n", i);
 		}
 		i++;
 	}
 	if ((total_connections != 6 || total_connections != 8) && hash_counter != 4)
 	{
-		//ft_putstrcolor("shape_wrong\n", "red");
+		ft_putstrcolor("shape_wrong\n", "red");
 			return (0);
 	} // add the pieces if its valid
 	else
-		//ft_putstrcolor("shape_right\n", "green");
+		ft_putstrcolor("shape_right\n", "green");
 	return (1);
 }
 
@@ -89,18 +89,19 @@ int	reader(char *str, int fd)
 	ret = read(fd, &buffer, BUFF_SIZE);
 	while (ret)
 	{
-		//printf("\n\n\n\n\n\n\n%s", buffer);
-		//printf("%d\n", BUFF_SIZE);
+		printf("\n\n\n\n\n\n\n%s", buffer);
+		printf("%d\n", BUFF_SIZE);
 		buffer[ret] = '\0';
 		if (!(validate(buffer, ret)))
 		{
-			ft_putstrcolor("Invalid!\n", "red");
+			return (0);
+			//ft_putstrcolor("Invalid!\n", "red");
 		}
-		else
-			ft_putstrcolor("Valid!\n", "green");
+		//else
+			//ft_putstrcolor("Valid!\n", "green");
 		ret = read(fd, &buffer, BUFF_SIZE);
 	}
-	return(0);
+	return(1);
 }
 
 int	main(int argc, char **argv)
@@ -114,23 +115,23 @@ int	main(int argc, char **argv)
 	{
 		if (argc < 2)
 		{
-			ft_putstr("error");
+			ft_putstrcolor("error!\n", "red");
 			exit(EXIT_FAILURE);
 		}
 		if (argc > 2)
 		{
-			ft_putstr("error");
+			ft_putstrcolor("error!\n", "red");
 			exit(EXIT_FAILURE);
 		}
 	}
 	if (reader(argv[1], fd))
 	{
-		ft_putstrcolor("error!\n", "red");
+		ft_putstrcolor("Sucess!\n", "green");
 		return (0);
 	}
 	else
 	{
-		ft_putstrcolor("Sucess!\n", "green");
+		ft_putstrcolor("error!\n", "red");
 		return (1);
 	}
 	if (close(fd) == -1)
